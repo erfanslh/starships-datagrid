de2644a6d7cfe6bee0652cdc79a77e8094a22ec8
erfanslh <erfan.slh@yahoo.com>
2026-05-17 01:50:39 +0200

    feat(service): add swapi service with pagination and page caching

---
+++ src/app/core/services/swapi.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Starship, SwapiPage } from '../models/starship.model';
+
+@Injectable({ providedIn: 'root' })
+export class SwapiService {
+  private http = inject(HttpClient);
+  // due to ssl certificate in swapi.dev i used an alternative apiUrl
+  private baseUrl = 'https://swapi.py4e.com/api';
+
+  private cache = new Map<number, Starship[]>();
+  totalCount = 0;
+
+  // return starships for the given page number
+  getPage(page: number): Observable<Starship[]> {
+    if (this.cache.has(page)) {
+      return of(this.cache.get(page)!);
+    }
+    return this.http.get<SwapiPage>(`${this.baseUrl}/starships/?page=${page}`).pipe(
+      map((response) => {
+        this.totalCount = response.count;
+        const starships = response.results.map((s) => ({ ...s, notes: '' }));
+        this.cache.set(page, starships);
+        return starships;
+      }),
+    );
+  }
+  // returns a list of all starships currently stored in cache.
+  getCached(): Starship[] {
+    return Array.from(this.cache.values()).flat();
+  }
+}
