681c92797c8d15897810b5cfe8ee0353e1aa4a50
erfanslh <erfan.slh@yahoo.com>
2026-06-10 12:30:06 +0200

    refactor: nest (config, components, and utils) inside starship-grid feature

--- src/app/features/starship-grid/starship-grid.ts
+++ src/app/features/starship-grid/starship-grid.ts
@@ -2,10 +2,10 @@ import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { GridApi, GridReadyEvent, IGetRowsParams } from 'ag-grid-community';
 import { SwapiService } from './../../core/services/swapi.service';
-import { STARSHIP_COLUMN_DEFS, STARSHIP_DEFAULT_COL_DEF} from '../config/column-defs';
-import { STARSHIP_GRID_THEME } from '../config/grid-theme';
-import { applySort } from '../utils/sort.utils';
-import { SearchBarComponent } from '../components/search-bar/search-bar';
+import { STARSHIP_COLUMN_DEFS, STARSHIP_DEFAULT_COL_DEF} from './config/column-defs';
+import { STARSHIP_GRID_THEME } from './config/grid-theme';
+import { applySort } from './utils/sort.utils';
+import { SearchBarComponent } from './components/search-bar/search-bar';
 
 @Component({
   selector: 'app-starship-grid',
